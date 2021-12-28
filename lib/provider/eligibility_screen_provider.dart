aac761726d4f0b0c690903249ea59c98e8115210
Anik Mosfeq <mosfeqanik01@gmail.com>
2021-12-28 13:43:09 +0600

    eligibility check provider added

---
+++ lib/provider/eligibility_screen_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class EligibilityScreenProvider extends ChangeNotifier {
+  String _message = '';
+  bool _isEligible= false;
+
+  String get message => _message;
+
+  bool get isEligible => _isEligible;
+
+  void checkIsEligibility(int age){
+    if(age>=18){
+      eligible();
+    }else{
+      notEligible();
+    }
+  }
+  void eligible() {
+    _isEligible=true;
+    _message='You are eligible to apply driver license';
+    notifyListeners();
+  }
+  void notEligible() {
+    _isEligible=false;
+    _message='You are not eligible to apply driver license';
+    notifyListeners();
+  }
+
+}
+
+
